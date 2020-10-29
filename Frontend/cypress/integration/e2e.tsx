8b05fb2e6ffd6f6e61dc80bbe0fd3f2d53e3e92f
Ingvild Telle Finsås <ingvildtf@Ingvilds-MacBook.local>
2020-10-29 20:54:36 +0100

    Added cypress

---
+++ Frontend/cypress/integration/e2e.tsx
@@ -0,0 +1,16 @@
+export{}
+import {} from "cypress";
+
+describe('End to end test', () => {
+
+
+    beforeEach(() => {
+        cy.visit('http://localhost:3000');
+      });
+    
+      it('Front page loaded', () => {
+        
+      });
+    
+
+});
