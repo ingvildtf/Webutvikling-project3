a0020f87d008670422bd7d6b054a405eca79f70b
Agnes Marie Ødegård Ockernahl <amockern@stud.ntnu.no>
2020-10-29 21:03:54 +0100

    Added queries in frontend

--- Frontend/src/queries.ts
+++ Frontend/src/queries.ts
@@ -12,3 +12,52 @@ export const GET_RECIPE_QUERY = gql`
     }
   }
 `
+
+export const GET_DINNER_RECIPES = gql`
+  query Dinners($offset: Int, $limit: Int) {
+    dinner(limit: $limit, offset: $offset) {
+      ID
+      Name
+      Category
+      Instruction
+      Ingredients
+      Image
+  }
+}`
+
+export const GET_Dessert_RECIPES = gql`
+  query Desserts($offset: Int, $limit: Int) {
+    dessert(limit: $limit, offset: $offset) {
+      ID
+      Name
+      Category
+      Instruction
+      Ingredients
+      Image
+  }
+}`
+
+export const GET_BREAKFAST_RECIPES = gql`
+  query Breakfasts($offset: Int, $limit: Int) {
+    breackfast(limit: $limit, offset: $offset) {
+      ID
+      Name
+      Category
+      Instruction
+      Ingredients
+      Image
+  }
+}`
+export const SEARCH_RECIPES = gql`
+  query SearchRecipesQuery($matchedString: String!) {
+    searchRecipes(searchSequence: $matchedString) {
+      ID
+      Name
+      Category
+      Instruction
+      Ingredients
+      Image
+  }
+}`
+
+
