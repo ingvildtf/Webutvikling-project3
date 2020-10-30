d938ac88f5e2497928a8398e0c2ba433d273eb83
Cathrine Bartnes <cathrine2410@gmail.com>
2020-10-30 10:09:22 +0100

    Style components

--- Frontend/src/components/modal/modal.style.tsx
+++ Frontend/src/components/modal/modal.style.tsx
@@ -7,7 +7,10 @@ export const WrapperModal = styled.div`
   left: 50%;
   transform: translate(-50%, -50%);
   z-index: 700;
-  width: 70vw;
+  width: 80vw;
+  // height: 60vh;
+  background-color: #f2f2f2;
+  border: 1px solid #607878;
 
   display: grid;
   grid-template-rows: min-content min-content auto;
@@ -19,8 +22,10 @@ export const WrapperModal = styled.div`
 
   @media screen and (max-width: 800px) {
     width: auto;
+    top: 50%;
+    height: 85vh;
     grid-template-columns: auto;
-    grid-template-rows: min-content min-content auto auto;
+    grid-template-rows: min-content min-content min-content 1fr;
     grid-template-areas:
       'title'
       'img'
@@ -35,7 +40,7 @@ export const Backdrop = styled.div`
   position: fixed;
   top: 0;
   left: 0;
-  background: rgba(0, 0, 0, 0.3);
+  background: rgba(0, 0, 0, 0.4);
   z-index: 500;
 `
 
@@ -45,8 +50,9 @@ export const Header = styled.div`
   justify-content: space-between;
   padding: 10px;
   background-color: #afc9be;
-  color: #607878;
+  color: white;
   font-size: 18px;
+  border-bottom: 2px dotted #607878;
 `
 
 export const CloseButton = styled.button`
@@ -61,10 +67,10 @@ export const CloseButton = styled.button`
 `
 export const Picture = styled.img`
   grid-area: img;
-  width: 30vw;
+  width: 20vw;
   height: auto;
-  padding: 5vw;
-  background-color: white;
+  padding: 2vw;
+  background-color: #f2f2f2;
   @media screen and (max-width: 800px) {
     justify-self: center;
     width: 70vw;
@@ -73,15 +79,20 @@ export const Picture = styled.img`
 
 export const Content = styled.div`
   grid-area: cont;
-  padding: 5vw;
+  padding: 2vw;
   overflow: hidden;
-  background-color: white;
-  color: #607878;
+  background-color: #f2f2f2;
+  color: black;
+  display: flex;
+  align-items: center;
 `
 
 export const Recipe = styled.div`
   grid-area: recipe;
-  padding: 5vw;
-  background-color: white;
-  color: #607878;
+  padding: 2vw;
+  border-top: 2px dotted #607878;
+  max-height: 100%;
+  background-color: #f2f2f2;
+  color: black;
+  overflow: auto;
 `
